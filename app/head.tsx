e4bbac2dcac858c35fd409b982c73caed7f352d6
EmpresaCFTDROP <empresacftcalcadosmkp@gmail.com>
2025-04-24 16:23:05 -0300

    Create head.tsx

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>CFT DROP - Dropshipping de Calçados</title>
+      <meta name="description" content="Venda calçados sem estoque e sem investimento inicial." />
+      <script src="//code.tidio.co/hcb4iqisxpo8etghga2olwe79byjyss1.js" async></script>
+    </>
+  )
+}
